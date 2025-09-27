31613511fab0f75349b00c64f11786ff46ea3963
steven giraldo <stevengiraldotoro@gmail.com>
2025-09-26 19:30:41 -0500

    se crea sout con mi nombre en el MAIN FEAT

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("hola soy steven giraldo");
 
     }
 }
